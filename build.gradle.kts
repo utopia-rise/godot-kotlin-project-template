702dddba01c15ad06aa32a69645134ec4aae544c
Ced Naru <cednaru.dev@gmail.com>
2024-09-29 23:39:06 +0200

    Update to 0.10.0-4.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.utopia-rise.godot-kotlin-jvm") version "0.9.1-4.2.2"
+    id("com.utopia-rise.godot-kotlin-jvm") version "0.10.0-4.3.0"
 }
 
 repositories {
@@ -9,12 +9,41 @@ repositories {
 }
 
 godot {
+    // ---------Setup-----------------
+
+    // the script registration which you'll attach to nodes are generated into this directory
     registrationFileBaseDir.set(projectDir.resolve("scripts"))
-    isRegistrationFileHierarchyEnabled.set(true)
-	
-	// To enable Android Export.
-    //isAndroidExportEnabled.set(true) 
-	
-	// To enable iOS export and Graal Native Image export.
-    //isGraalNativeImageExportEnabled.set(true)
+
+    // Create .gdj files from all JVM scripts
+    isRegistrationFileGenerationEnabled.set(true)
+
+    // defines whether the script registration files should be generated hierarchically according to the classes package path or flattened into `registrationFileBaseDir`
+    //isRegistrationFileHierarchyEnabled.set(true)
+
+    // defines whether your scripts should be registered with their fqName or their simple name (can help with resolving script name conflicts)
+    //isFqNameRegistrationEnabled.set(false)
+
+    // ---------Android----------------
+
+    // NOTE: Make sure you read: https://godot-kotl.in/en/stable/user-guide/exporting/#android as not all jvm libraries are compatible with android!
+    // IMPORTANT: Android export should to be considered from the start of development!
+    //isAndroidExportEnabled.set(ANDROID_ENABLED)
+    //d8ToolPath.set(File("D8_TOOL_PATH"))
+    //androidCompileSdkDir.set(File("ANDROID_COMPILE_SDK_DIR"))
+
+    // --------IOS and Graal------------
+
+    // NOTE: this is an advanced feature! Read: https://godot-kotl.in/en/stable/user-guide/advanced/graal-vm-native-image/
+    // IMPORTANT: Graal Native Image needs to be considered from the start of development!
+    //isGraalNativeImageExportEnabled.set(IS_GRAAL_VM_ENABLED)
+    //graalVmDirectory.set(File("GRAAL_VM_DIR"))
+    //windowsDeveloperVCVarsPath.set(File("WINDOWS_DEVELOPER_VS_VARS_PATH"))
+    //isIOSExportEnabled.set(IS_IOS_ENABLED)
+
+    // --------Library authors------------
+
+    // library setup. See: https://godot-kotl.in/en/stable/develop-libraries/
+    //classPrefix.set("MyCustomClassPrefix")
+    //projectName.set("LibraryProjectName")
+    //projectName.set("LibraryProjectName")
 }
